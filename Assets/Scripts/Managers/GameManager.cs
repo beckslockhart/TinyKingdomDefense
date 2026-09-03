b42411850c67ef46e6dd7753d30e1496bc08f469
beckslockhart <165903789+beckslockhart@users.noreply.github.com>
2026-09-03 09:20:32 +0200

    pause menu + restart controls

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 
@@ -6,10 +7,13 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    [Header("UI Panels")]
     [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject pausePanel;
     [SerializeField] private GameObject hudPanel;
 
     public bool IsGameOver { get; private set; }
+    public bool IsPaused { get; private set; }
 
     
     private void Awake()
@@ -17,15 +21,57 @@ public class GameManager : MonoBehaviour
         Instance = this;
         Time.timeScale = 1f;
 
-        if (gameOverPanel != null)
+        gameOverPanel.SetActive(false);
+        pausePanel.SetActive(false);
+        hudPanel.SetActive(true);
+    }
+
+    
+    private void Update()
+    {
+        if (Keyboard.current == null || IsGameOver)
         {
-            gameOverPanel.SetActive(false);
+            return;
         }
-        
-        if (hudPanel != null)
+
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
         {
-            hudPanel.SetActive(true);
+            TogglePause();
         }
+    }
+
+    
+    public void TogglePause()
+    {
+        if (IsPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    
+    public void PauseGame()
+    {
+        if (IsGameOver)
+        {
+            return;
+        }
+
+        IsPaused = true;
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    
+    public void ResumeGame()
+    {
+        IsPaused = false;
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
     }
 
     
@@ -37,16 +83,11 @@ public class GameManager : MonoBehaviour
         }
 
         IsGameOver = true;
+        IsPaused = false;
 
-        if (gameOverPanel != null)
-        {
-            gameOverPanel.SetActive(true);
-        }
-        
-        if (hudPanel != null)
-        {
-            hudPanel.SetActive(false);
-        }
+        pausePanel.SetActive(false);
+        hudPanel.SetActive(false);
+        gameOverPanel.SetActive(true);
 
         Time.timeScale = 0f;
     }
